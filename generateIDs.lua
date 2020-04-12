f13501b350131ba16f41395745b53e3df25eebd5
Professor Cupcake <profcupcake@hotmail.co.uk>
2020-04-12 13:03:01 +0100

    generateIDs.lua:
     - Added support for local (non-workshop) mods
     - IDs and names are now collated into a table and concatenated into a single string at the end of the script

--- generateIDs.lua
+++ generateIDs.lua
@@ -11,6 +11,7 @@ Both arguments are optional; they will default to "input.html" and "output.txt".
 
 BE AWARE: There's no input validation; if you give it bad arguments, it will fuck up and it'll be all your fault. 
 Also note that some manual verification may be required, as the folder names taken from the preset file are not guaranteed to match those that are actually created for the mods. They also may have esoteric HTML escapes that are not caught by the script. 
+Also also: local mods that use the bar (|) may break the name detection.
 
 by Professor Cupcake
 ]]
@@ -30,21 +31,33 @@ end
 
 l = f:read("*line")
 
-local idString = ""
-local nameString = ""
+local idList = {}
+local nameList = {}
 
 while l do
 	if string.find(l, [[<tr data%-type="ModContainer"]]) then
 		repeat
 			l = f:read("*line")
-		until string.find(l, [[<td data%-type="DisplayName">]])
+		until string.find(l, [[<td data%-type="DisplayName]])
 		local modName = string.match(l, [[<td data%-type="DisplayName">(.+)</td>]])
-		nameString = nameString.."@"..modName..";"
 		repeat
 			l = f:read("*line")
-		until string.find(l, "<a href=")
-		local modId = string.match(l, [[<a href="http://steamcommunity%.com/sharedfiles/filedetails/%?id=(%d+)"]])
-		idString = idString..modId..","
+		until string.find(l, [[<span class="from]])
+		local from = string.match(l, [[<span class="from%-(%a+)">]])
+		if from == "local" then
+			repeat
+				l = f:read("*line")
+			until string.find(l, [[<span class="whups" data%-type="Link"]])
+			modName = string.match(l, [[<span class="whups" data%-type="Link" data%-meta="local:.-|(.-)|]])
+			table.insert(nameList, modName)
+		elseif from == "steam" then
+			table.insert(nameList, "@"..modName)
+			repeat
+				l = f:read("*line")
+			until string.find(l, [[<a href="http://steamcommunity%.com]])
+			local modId = string.match(l, [[<a href="http://steamcommunity%.com/sharedfiles/filedetails/%?id=(%d+)"]])
+			table.insert(idList, modId)
+		end
 	end
 	l = f:read("*line")
 end
@@ -64,7 +77,10 @@ function unescape(str)
 	return str
 end
 
+nameString = table.concat(nameList, ";")
 nameString = unescape(nameString);
+
+idString = table.concat(idList, ",")
 
 f = assert(io.open(output, 'a+'))
 f:write("IDs from \"",input,"\"\n",idString,"\n\n",nameString,"\n\n")
